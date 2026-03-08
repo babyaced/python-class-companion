dd94956d8e8b3455976946a7a3ad67bd5875ec9a
Daniel Simpson <dasimps@gap.com>
2026-03-08 10:43:52 -0700

    add conditionals under control flow

--- content/zh/ControlFlow/_meta.jsx
+++ content/zh/ControlFlow/_meta.jsx
@@ -1,11 +1,9 @@
-export default{
-    IfStatements: 'If Statements',
-    ComparisonOperators: 'Comparison Operators',
-    LogicalOperators: 'Logical Operators',
-    Loops: 'Loops',
-    MatchCase: 'Match Statements',
-    TernaryStatements: {
-        title: "Ternary Statements",
-        display: "hidden"
-    }
+export default {
+  Conditionals: 'Conditionals',
+  Loops: 'Loops',
+  MatchCase: 'Match Statements',
+  TernaryStatements: {
+    title: 'Ternary Statements',
+    display: 'hidden',
+  },
 }
